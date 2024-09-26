bd31095f741d450b1a4f3187d0ed2c3db27e0c2f
Felipe Pinto <linkrag.felipe@gmail.com>
2024-09-26 18:54:57 -0300

    Better CSS and new feature
    Add option to use as password input
    Add crude text toggle

--- src/components/CustomFloatLabelInput.js
+++ src/components/CustomFloatLabelInput.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CustomFloatLabelInput.css';
 
-const CustomFloatLabelInput = ({ id, value, onChange, label, textarea }) => {
+const CustomFloatLabelInput = ({ id, value, onChange, label, textarea, type, onBlur }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const InputComponent = textarea ? 'textarea' : 'input';
+
+  const handleTogglePassword = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
 
   return (
     <div className={`custom-float-label-input ${textarea ? 'textarea' : ''}`}>
       <InputComponent
         id={id}
+        type={type === 'password' && !isPasswordVisible ? 'password' : 'text'}
         className={value ? 'active' : ''}
         value={value}
         onChange={onChange}
+        onBlur={onBlur}
+        required
       />
       <label htmlFor={id}>{label}</label>
+      {type === 'password' && (
+        <button type="button" onClick={handleTogglePassword} className="toggle-password">
+          {isPasswordVisible ? '👁️' : '👁️‍🗨️'}
+        </button>
+      )}
     </div>
   );
 };
